4b0aa774acc66c41572442ed5f0b387bc31d0ee3
amemura <aods1004@yahoo.co.jp>
2018-05-26 10:55:18 +0900

    ブログデータを更新する

--- src/Twig/Meta.php
+++ src/Twig/Meta.php
@@ -2,7 +2,6 @@
 
 namespace App\Twig;
 
-
 /**
  * Class ReversibleTemplate
  * @package App\Twig
@@ -18,10 +17,39 @@ class Meta extends \Twig_Extension
     {
 
         return [
-            new \Twig_SimpleFunction('app_entry', function ($data) {
-                $this->meta = $data;
+            new \Twig_Function('app_entry', function ($data) {
+                $this->meta = array_merge($this->meta, $data);
                 return '';
             }),
+            new \Twig_Function('app_template', function ($name = null) {
+                ob_start();
+                require __DIR__."/../../../templates/$name.php";
+                return '<!-- #auto-generated-by-from -->'
+                    . implode('', array_map('trim', explode("\n", ob_get_clean())))
+                    . '';
+            }),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_Filter('meta_data', function ($value) {
+                return str_replace("'", "\'", $value);
+                }, ['is_safe' =>['html']]),
+            new \Twig_Filter('md_content', function ($value) {
+                return strtr($value, [
+                    "{{" => "{{ '{{' }}",
+                    "}}" => "{{ '}}' }}",
+                    "{#" => "{{ '{#' }}",
+                    "#}" => "{{ '#}' }}",
+                    "{%" => "{{ '{%' }}",
+                    "%}" => "{{ '%}' }}",
+                ]);
+            }, ['is_safe' =>['html']]),
         ];
     }
 
